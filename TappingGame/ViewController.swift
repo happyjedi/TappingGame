2fe1935f883357d8340d63a8e80d7a97699472ce
happyjedi <exploit.vn@yandex.ru>
2016-01-05 15:35:25 +0300

    Added init conditions and vars

--- TappingGame/ViewController.swift
+++ TappingGame/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timeInt = 0
+    var tapInt = 0
+    
+    @IBOutlet weak var TimeLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
+    
+    @IBOutlet weak var TapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timeInt = 10
+        tapInt = 0
+        
+        TimeLabel.text = String(timeInt)
+        ScoreLabel.text = String(tapInt)
+        
+        TapButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func StartGame(sender: AnyObject) {
+    }
 
 
+    @IBAction func TapMe(sender: AnyObject) {
+    }
+    
 }
 
